676cace07bf0726e9bf11a9876c8d2b951c654ca
Kyrosta <georgiusm07@gmail.com>
2024-04-21 23:02:44 +0700

    FINAL 2.0!

--- app/src/main/java/com/leon/hobbyapp/view/MainActivity.kt
+++ app/src/main/java/com/leon/hobbyapp/view/MainActivity.kt
@@ -57,6 +57,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
