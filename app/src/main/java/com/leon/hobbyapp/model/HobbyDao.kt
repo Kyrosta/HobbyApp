6ad69f6acb0ea3a26bb24804d0c5e5516b5bcd61
hszhrr <ffelicia918@gmail.com>
2024-06-27 13:59:42 +0700

    hobby dao

---
+++ app/src/main/java/com/leon/hobbyapp/model/HobbyDao.kt
@@ -0,0 +1,26 @@
+package com.leon.hobbyapp.model
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface HobbyDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg hobby: Hobby)
+
+    @Query("SELECT * FROM hobby")
+    fun selectAllHobby(): List<Hobby>
+
+    @Query("SELECT * FROM hobby WHERE uuid= :id")
+    fun selectHobby(id:Int): Hobby
+
+    @Delete
+    fun deleteHobby(hobby:Hobby)
+
+    @Update
+    fun updateHobby(hobby:Hobby)
+}
